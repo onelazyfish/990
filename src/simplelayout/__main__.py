122c8ec3b9b4c734b0f7aadc4215ca363c547009
onelazyfish <32604228@qq.com>
2021-11-22 22:10:04 +0800

    first commit

--- src/simplelayout/__main__.py
+++ src/simplelayout/__main__.py
@@ -1,8 +1,8 @@
 # TODO 正确导入函数 generate_matrix, save_matrix, save_fig
 
-from cli import get_options  # TODO: 保证不修改本行也可以正确导入
-from generator.core import generate_matrix
-from generator.utils import save_matrix, save_fig, make_dir
+from simplelayout.cli import get_options  # TODO: 保证不修改本行也可以正确导入
+from simplelayout.generator.core import generate_matrix
+from simplelayout.generator.utils import save_matrix, save_fig, make_dir
 
 
 def main():
